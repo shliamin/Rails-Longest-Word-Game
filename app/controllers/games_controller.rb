ff0d6635437ee91629444933173a891bec8a8941
Efim <efimshliamin@gmail.com>
2024-06-19 07:19:14 +0200

    Update styles and add button to generate new letters

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
-require "open-uri"
+require 'net/http'
+require 'json'
 
 class GamesController < ApplicationController
   def new
@@ -6,33 +7,36 @@ class GamesController < ApplicationController
   end
 
   def score
-    @letters = params[:letters].split()
+    @letters = params[:letters].split
     @score = params[:longest_word].upcase.split("")
     @score.each do |element1|
-
-      @letters.include?(element1) ? @answer = valid_word : @answer = "Sorry, but #{@score.join} can not be built out of #{@letters}"
-
-
+      if @letters.include?(element1)
+        @answer = valid_word
+      else
+        @answer = "Sorry, but #{@score.join} can not be built out of #{@letters}"
+        break
+      end
     end
   end
 
   def check?(score)
-    url = "https://wagon-dictionary.herokuapp.com/#{score}"
-    json = open(url).read
-    object = JSON.parse(json)
-    object["found"]
+    url = "https://api.dictionaryapi.dev/api/v2/entries/en/#{score}"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    object = JSON.parse(response)
+    object.is_a?(Array) && object.first["word"].present?
   end
 
   def valid_word
-      if @check = check?(@score.join.downcase)
-        @english_word = "okay, this is a valid English word"
-      else
-        @english_word = "Sorry, but #{@score.join} does not seem to be a valid English word"
-      end
+    if check?(@score.join.downcase)
+      @english_word = "okay, this is a valid English word"
+    else
+      @english_word = "Sorry, but #{@score.join} does not seem to be a valid English word"
+    end
   end
 
-def index
-  current_user = User.find_by_id(session[:current_user_id])
-  # ...
-end
+  def index
+    current_user = User.find_by_id(session[:current_user_id])
+    # ...
+  end
 end
---
+++ spec/models/example_spec.rb
@@ -0,0 +1,8 @@
+# spec/models/example_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Example test' do
+  it 'works correctly' do
+    expect(1 + 1).to eq(2)
+  end
+end
